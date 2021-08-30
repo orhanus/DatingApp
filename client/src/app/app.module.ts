f3b1a73fc3e430dfd3c5b0894af8bbc987711c03
orhanus <89778334+orhanus@users.noreply.github.com>
2021-08-30 15:43:36 +0200

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
+
+//bukvalno bezveze da imam sta pullat
 @NgModule({
   declarations: [
     AppComponent,
