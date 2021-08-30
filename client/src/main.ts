afba53f065b49e80ebd6321493973e73c966b02e
orhanus <orhanus@maestralsolutions.com>
2021-08-30 16:27:22 +0200

    Testing

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+//bilo kakva promjena
 if (environment.production) {
   enableProdMode();
 }
