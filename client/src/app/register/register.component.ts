6bd469b15c2e4a3721a1bea181d6e299b8407e86
orhanus <orhanus@maestralsolutions.com>
2021-09-01 17:28:09 +0200

    Added a date picker with validation to registration form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -12,16 +12,24 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter() 
   model: any = {};
   registerForm: FormGroup;
+  maxDate: Date;
 
   constructor(private accountService: AccountService, private toastr: ToastrService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   initializeForm(){
     this.registerForm = this.formBuilder.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
 
